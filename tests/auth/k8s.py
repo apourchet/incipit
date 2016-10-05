5a1c8dfba35f44982f2a4bb6d536a886edd714ae
Antoine Pourchet <antoine@brkt.com>
2016-10-05 09:31:13 -0700

    Revamp well under way, removed a bunch

---
+++ tests/auth/k8s.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+class Auth():
+    def __init__(self):
+        pass
+
+    def test(self):
+        print "tested"
+        pass
+
+if __name__ == "__main__":
+    auth = Auth()
+    auth.test()
